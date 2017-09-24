af4b78b92c63a7b350eb83bf724f8c530e204068
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2017-09-24 23:10:27 +0100

    Getting to find out how to draw the SVG :)

--- src/Weather/components/chart.vue
+++ src/Weather/components/chart.vue
@@ -1,9 +1,8 @@
 <template>
-	<svg class="windSpeedChart" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-		
+	
+
+	<svg class="windSpeedChart" width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="none"> 
 		<path :d="generatePath" stroke="red" stroke-width="2" fill="none"></path>
-			
-		
 	</svg>
 </template>
 
@@ -18,9 +17,13 @@
 			// @todo: this needs to relate to the width of wind bar, make windBar send back it's width in pixels to store?
 			// @todo: finish this :) https://css-tricks.com/svg-path-syntax-illustrated-guide/
 			generatePath () {
-				let path = "M0,25 ";
+				let path = "M0,75 ";
+				let i = 1;
 				this.weather.forEach(function(hour) {
-					path = path + "C50,50 50,50 100,75"; // @todo: build the path string
+					let coordinates = (75 + i * 50).toString();
+					let speed = (hour.windSpeed * 5).toFixed(0);
+					path += " L " + speed + ", " + coordinates; // @todo: build the path string
+					i++;
 				});
 				console.log(path);
 				return path;
