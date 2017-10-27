aa847e75aca15be0754bdff826a1b8aeef96557a
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2017-10-27 23:40:35 +0200

    List of places

--- src/Place/UserPlacesView/index.vue
+++ src/Place/UserPlacesView/index.vue
@@ -1,28 +1,38 @@
 <template>
 	<div class="">
-		My places
-		<br>
-		<ul>
-			<li v-for="place in places">{{place.placeName}} <br> {{place}}</li>
+		<div class="mainNav">
+			<h1 class="mainNav__title">My places</h1>
+		</div>
+		
+		<ul class="placesNavigator">
+			<li v-for="place in places">
+				<router-link :to="place.placeSlug">
+					<span class="placesNavigator">
+						{{place.placeName}}
+					</span>
+				</router-link>
+			</li>
 		</ul>
 	</div>
 </template>
 <script>
-    import Vue from 'vue';
+	import Vue from 'vue';
 		import {HTTP} from '../../http-common';
 
 
-    export default {
-        name: 'UserPlacesView',
+	export default {
+		name: 'UserPlacesView',
 
 				created () {
 					// @load places also when component is re rendered AND when user has just logged in
 					this.loadPlacesData ()
 				},
-        computed: {
+		computed: {
+			userName () {
+				return this.$route.params.username;
+			}
 
-
-        },
+		},
 				methods: {
 
 					loadPlacesData () {
@@ -50,7 +60,7 @@
 				}
 
 
-    }
+	}
 
 
 
