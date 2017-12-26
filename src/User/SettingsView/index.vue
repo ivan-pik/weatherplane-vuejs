b29866ef1fe8e454814f7a87eb821d1dc5491fb7
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2017-12-26 21:14:56 +0100

    settings todo

--- src/User/SettingsView/index.vue
+++ src/User/SettingsView/index.vue
@@ -2,10 +2,23 @@
 	<div class="">
 		Settings View
 
+		@todo: use browser locales to get the best initial Setting
+		@todo: note that these need to work for logged out / new places as well
+
 		@todo: Temp unit F / C
+
 		@todo: Wind unit m/s / km/h  /  mph
 
+		@todo: Change password
+
+		@todo: change email
+
+		@todo: 12/24 time format
+
+		@todo: Date format
 		
+
+
 
 	</div>
 
