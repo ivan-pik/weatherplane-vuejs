aec95d737ffe8226e1b5dd46dd5b42ad97d8ab40
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2017-12-26 20:31:16 +0100

    Settings View todo

--- src/User/SettingsView/index.vue
+++ src/User/SettingsView/index.vue
@@ -1,21 +1,26 @@
 <template>
-    <div class="">
-        Settings View
-    </div>
+	<div class="">
+		Settings View
+
+		@todo: Temp unit F / C
+		@todo: Wind unit m/s / km/h  /  mph
+
+		
+
+	</div>
 
 </template>
 <script>
-    import Vue from 'vue'
+	import Vue from 'vue'
 
-    export default {
-        name: 'SettingsView',
+	export default {
+		name: 'SettingsView',
 
-        data() {
-            return {
+		data() {
+			return {
 
-            }
-        }
-    }
+			}
+		}
+	}
 
 </script>
-<style scoped></style>
