0068feebb0c9b87ef45673eb64096dbf0180a533
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2018-01-21 21:22:52 +0000

    Spaces to tabs and setting up aliases for modules and CSS

--- webpack.config.js
+++ webpack.config.js
@@ -6,101 +6,109 @@ var autoprefixer = require('autoprefixer');
 
 
 const extractSass = new ExtractTextPlugin({
-    filename: '[name].css'
+		filename: '[name].css'
 });
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      },
-		  {
-        test: /\.scss$/,
-        loader: extractSass.extract({
-            use: [
-                {
-                    loader: 'css-loader',
-                    options: {
-                      sourceMap: true,
-                      importLoaders: 1
-                    },
-                },
-                // Enables autoprefixer & other configs -- check postcss.config file
-                {
-                    loader: 'postcss-loader',
-                    options: {
-                        sourceMap: true
-                    }
-                },
-                // Let's us work with relative urls in css
-                {
-                    loader: 'resolve-url-loader',
-                    options: {
-                        sourceMap: true
-                    }
-                },
-                // Loads SASS files
-                {
-                    loader: 'sass-loader',
-                    options: {
-                        sourceMap: true
-                    }
-                }
-            ],
-                // Use style-loader in development
-                fallback: 'style-loader',
-            })
-        }
-    ]
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    watchOptions: {
-      ignored: /node_modules/
-    }
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map',
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: '/dist/',
+		filename: 'build.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					loaders: {
+						// Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+						// the "scss" and "sass" values for the lang attribute to the right configs here.
+						// other preprocessors should work out of the box, no loader config like this necessary.
+						'scss': 'vue-style-loader!css-loader!sass-loader',
+						'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+					}
+					// other vue-loader options go here
+				}
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(png|jpg|gif|svg)$/,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]?[hash]'
+				}
+			},
+			{
+				test: /\.scss$/,
+				loader: extractSass.extract({
+						use: [
+								{
+										loader: 'css-loader',
+										options: {
+											sourceMap: true,
+											importLoaders: 1
+										},
+								},
+								// Enables autoprefixer & other configs -- check postcss.config file
+								{
+										loader: 'postcss-loader',
+										options: {
+												sourceMap: true
+										}
+								},
+								// Let's us work with relative urls in css
+								{
+										loader: 'resolve-url-loader',
+										options: {
+												sourceMap: true
+										}
+								},
+								// Loads SASS files
+								{
+										loader: 'sass-loader',
+										options: {
+												sourceMap: true
+										}
+								}
+						],
+								// Use style-loader in development
+								fallback: 'style-loader',
+						})
+				}
+		]
+	},
+	resolve: {
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js',
+			// 'GlobalSass': path.resolve('../src/styles/theme.scss'),
+			// styles: path.resolve(__dirname, 'src/styles/'),
+			'globalVars': path.resolve(__dirname, 'src/styles/theme.scss')
+		},
+		modules: [
+			path.resolve('./src'),
+			path.resolve('./node_modules')
+		]
+	},
+
+	devServer: {
+		historyApiFallback: true,
+		noInfo: true,
+		watchOptions: {
+			ignored: /node_modules/
+		}
+	},
+	performance: {
+		hints: false
+	},
+	devtool: '#eval-source-map',
 	plugins: [
-    extractSass,
+		extractSass,
 		new ExtractTextPlugin("./src/public/css/styles.css"),
 		new webpack.LoaderOptionsPlugin({
 			test: /\.s?css$/,
@@ -115,23 +123,23 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    // @todo: it complains too much
-    // new webpack.optimize.UglifyJsPlugin({
-    //   sourceMap: true,
-    //   compress: {
-    //     warnings: false
-    //   }
-    // }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
+	module.exports.devtool = '#source-map'
+	// http://vue-loader.vuejs.org/en/workflow/production.html
+	module.exports.plugins = (module.exports.plugins || []).concat([
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"'
+			}
+		}),
+		// @todo: it complains too much
+		// new webpack.optimize.UglifyJsPlugin({
+		//   sourceMap: true,
+		//   compress: {
+		//     warnings: false
+		//   }
+		// }),
+		new webpack.LoaderOptionsPlugin({
+			minimize: true
+		})
+	])
 }
