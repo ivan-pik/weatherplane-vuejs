355723a6278d014d7c9a81bb4c1f0d6f3135065f
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2018-02-25 16:41:42 +0000

    Make ResetPassword view great again

--- src/User/ResetPassword/index.vue
+++ src/User/ResetPassword/index.vue
@@ -1,110 +1,139 @@
 <template>
-    <div class="modal">
+	<div class="viewWrapper">
+		<navigation-header />
+		<div class="viewWrapper__scroller">
+			<div class="uiMessage uiMessage--error" v-if="tokenInvalid">
+				The password reset link has expired.
+				@todo: Go to retrieve your password again
+			</div>
 
-        <div v-if="tokenInvalid">
-            The password reset link has expired.
-        </div>
+			<div v-if="passwordResetSent">
+				<div class="uiMessage uiMessage--success" v-if="tokenInvalid">
+					Your password has been reset
+					@todo: Login here
+				</div>
+			</div>
 
-        <div v-if="passwordResetSent">
-            Your password has been reset
-        </div>
+			<form v-if="!passwordResetSent && !tokenInvalid" v-on:submit.prevent="onSubmit">
+				<div class="uiGroup">
 
-        <transition name="fade">
+					<h1 class="uiGroup__title">
+						Enter a new password
+					</h1>
+						
+					<div class="uiTextInputGroup">
+						<label class="uiLabel" for="password">Password</label>
+						<input
+							class="uiTextInput"
+							v-validate="{ rules: { required: true } }"
+							:class="{'input': true, 'is-danger': validationErrors.has('password') }"
+							name="password"
+							v-model="password"
+							type="password"
+							placeholder="Enter your password here"
+							autofocus
+							data-vv-delay="1000"
+						>
+						<span
+							v-show="validationErrors.has('password')"
+							class="uiHelp uiHelp--danger">{{ validationErrors.first('password') }}
+						</span>
+					</div>
 
-            <form v-if="!passwordResetSent && !tokenInvalid" v-on:submit.prevent="onSubmit">
+					<div class="uiTextInputGroup">
+						<label class="uiLabel" for="confirmPassword">Confirm Password</label>
+						<input
+							class="uiTextInput"
+							v-validate="{ rules: { required: true, confirmed:'password' } }"
+							:class="{'input': true, 'is-danger': validationErrors.has('confirmPassword') }"
+							name="confirmPassword"
+							v-model="confirmPassword"
+							type="password"
+							placeholder="Enter your password here"
+							autofocus
+							data-vv-delay="1000"
+						>
+						<span
+							v-show="validationErrors.has('confirmPassword')"
+							class="uiHelp uiHelp--danger">
+							{{ validationErrors.first('confirmPassword') }}
+						</span>
 
-                <label class="label" for="password">Password</label>
-                <br>
-                <input
-                        v-validate="{ rules: { required: true } }"
-                        :class="{'input': true, 'is-danger': validationErrors.has('password') }"
-                        name="password"
-                        v-model="password"
-                        type="password"
-                        placeholder="Enter your password here"
-                        autofocus
-                        data-vv-delay="1000"
-                >
-                <br>
-                <span
-                        v-show="validationErrors.has('password')"
-                        class="help is-danger">{{ validationErrors.first('password') }}
-                </span>
+					</div>
 
+					<!-- @todo: Delay the validation till the first field is filled -->
+					
+					<button class="uiButton uiButton--primary" type="submit">Continue</button>
+				</div>
+			</form>
 
-
-                <br>
-
-                <!-- @todo: Delay the validation till the first field is filled -->
-
-                <label class="label" for="confirmPassword">Confirm Password</label>
-                <br>
-                <input
-                        v-validate="{ rules: { required: true, confirmed:'password' } }"
-                        :class="{'input': true, 'is-danger': validationErrors.has('confirmPassword') }"
-                        name="confirmPassword"
-                        v-model="confirmPassword"
-                        type="password"
-                        placeholder="Enter your password here"
-                        autofocus
-                        data-vv-delay="1000"
-                >
-                <br>
-                <span
-                        v-show="validationErrors.has('confirmPassword')"
-                        class="help is-danger">
-                    {{ validationErrors.first('confirmPassword') }}
-
-                </span>
-
-
-                <br>
-                <button type="submit">Continue</button>
-            </form>
-
-        </transition>
-    </div>
+		</div>
+	</div>
 </template>
+
 <script>
-    import Vue from 'vue'
+	import WPAPI from 'wpapi/index';
+	import {errorCodeParser} from 'libs/errorCodeParser.js';
 
-    export default {
-        name: 'ResetPassword',
-        methods: {
-            onSubmit(event) {
-                this.$validator.validateAll().then(() => {
-                    let payload = {
-                        userID :  this.$route.query.userID,
-                        password: this.password,
-                        token : this.$route.query.token
-                    };
-                    this.$http.post('login/reset-password/', payload)
-                        .then(response => {
-                            if (response.body.success) {
+	export default {
+		name: 'ResetPassword',
+		methods: {
+			onSubmit(event) {
+				this.$validator.validateAll().then(() => {
+					let payload = {
+						userID :  this.$route.query.userID,
+						password: this.password,
+						token : this.$route.query.token
+					};
 
-                                this.passwordResetSent = true;
-                                this.tokenInvalid = false;
+					WPAPI.resetPassword(
+						payload
+					).then(response => {
+						if (response.success) {
+							this.passwordResetSent = true;
+							this.tokenInvalid = false;
+						} else {
+							if (response.errors) {
+								errorCodeParser ('token-expired',response.errors)
+								.then(() => {
+									this.tokenInvalid = true;
+								})
+								.catch(() => {
+									
+								});
 
-                            }
-                        }).catch(err => {
-                        if(err.body.errors) {
-                            this.tokenInvalid = true;
-                        }
-                    });
-                }).catch(() => {
-                    // when form is invalid
-                });
-            }
-        },
-        data() {
-            return {
-                password: '',
-                confirmPassword: '',
-                passwordResetSent: false,
-                tokenInvalid: false
-            }
-        }
-    }
+								errorCodeParser ('token-is-invalid',response.errors)
+								.then(() => {
+									this.tokenInvalid = true;
+								})
+								.catch(() => {
+									
+								})
+							} else {
+								this.$store.commit('GLOBAL_ADD_MESSAGE', {
+									text: 'Ooops, something went wrong',
+									type: 'error',
+								});
+							}
+						}
+					}).catch(err => {
+						// @todo:network error
+					})
+					
+				}).catch(() => {
+					// when form is invalid
+				});
+			}
+		},
+		data() {
+			return {
+				password: '',
+				confirmPassword: '',
+				passwordResetSent: false,
+				tokenInvalid: false
+			}
+		}
+	}
 
 </script>
 <style scoped></style>
