bbde3923d6d94e9cdf46c3780b775f7ad6498004
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2018-02-25 16:42:03 +0000

    WelcomeView - Pass places to the PlacesList

--- src/WelcomeView/index.vue
+++ src/WelcomeView/index.vue
@@ -2,30 +2,38 @@
 	<div class="viewWrapper">
 		<navigation-header />
 
-		<div v-if="!loggedIn" class="viewWrapper__scroller">
-			<p>Hi, blah blah blah blaaaahblah</p>
-			@Todo: Quick slider intro!
+		<div class="viewWrapper__scroller">
+			<div v-if="!loggedIn" class="">
+				<p>Hi, blah blah blah blaaaahblah</p>
+				@Todo: Quick slider intro!
 
-			<button
-				class="uiButton uiButton--primary"
-				@click="goToPlaceSearch"
-			>
-				Find my airfield!	
-			</button>
+				<button
+					class="uiButton uiButton--primary"
+					@click="goToPlaceSearch"
+				>
+					Find my airfield!	
+				</button>
 
-			<br>
+				<br>
 
-			<router-link class="uiLink" to="/login">Log In!</router-link>
+				<router-link class="uiLink" to="/login">Log In!</router-link>
+			</div>
+
+			<ui-loader
+				v-else
+				:loaded="!!places"
+				>
+				<places-list
+					:places="places"
+				/>
+			</ui-loader>
 		</div>
-		<places-list v-else
-			:userName="username"
-		/>
-
 	</div>
 </template>
 
 <script>
 	import navigationHeader from 'Navigation/navigationHeader.vue'
+	import WPAPI from 'wpapi/index';
 	import placesList from 'Navigation/placesList.vue';
 
 	export default {
@@ -34,17 +42,38 @@
 			'navigation-header' : navigationHeader,
 			'places-list' : placesList,
 		},
+		mounted () {
+			if (this.username) {
+				this.loadPlacesData();
+			}
+		},
 		computed: {
 			loggedIn() {
 				return this.$store.state.user.loggedIn;
 			},
 			username() {
 				return this.$store.state.user.name;
+			},
+			places () {
+				return this.$store.state.user.places;	
 			},	
+		},
+		watch: {
+			username (username) {
+				if (username !== null) {
+					this.loadPlacesData();
+				}
+			}
 		},
 		methods: {
 			goToPlaceSearch () {
 				this.$router.push('search');
+			},
+			loadPlacesData () {
+				WPAPI.getUserPlaces(this.username)
+				.then(places => {
+					this.$store.dispatch('USER_GET_PLACES', places);
+				});
 			}
 		},
 		data () {
