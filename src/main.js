472c6114ad3fbac5714d347fe773d2586c8ac061
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2018-03-07 20:16:13 +0000

    Remove a route to PlaceSettingsView - it will only be a popup

--- src/main.js
+++ src/main.js
@@ -15,7 +15,6 @@ import ResetPassword from './User/ResetPassword/index.vue'
 import SettingsView from './User/SettingsView/index.vue'
 import ExistingPlaceView from './Place/ExistingPlaceView/index.vue'
 import UserPlacesView from './Place/UserPlacesView/index.vue'
-import PlaceSettingsView from './Place/PlaceSettingsView/index.vue'
 
 import uiNote from 'uiComponents/uiNote.vue'
 import uiModal from 'uiComponents/modal.vue'
@@ -67,7 +66,6 @@ const routes = [
 		{ path: '/place', name: 'place', component: ExistingPlaceView },
 		{ path: '/:username/', component: UserPlacesView },
 		{ path: '/:username/:place', component: ExistingPlaceView },
-		{ path: '/:username/:place/settings', name: 'placeSettings', component: PlaceSettingsView }
 ]
 
 const router = new VueRouter({
