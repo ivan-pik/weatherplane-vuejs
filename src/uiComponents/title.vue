603b229a62bc81ed14b54d85d22cb018427e1717
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2018-03-10 20:56:38 +0000

    New uiTitle component

---
+++ src/uiComponents/title.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="uiTitle">
+		<slot />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'message',
+		data () {
+			return {
+				
+			}
+		}
+	}
+
+</script>
+
+<style lang="scss">
+	@import '~globalVars';
+	
+	.uiTitle {
+		text-align: center;
+		background: rgb(223, 108, 0);
+		color: #fff;
+		padding: 1em;
+		display: block;
+		font-size: 0.8rem;
+	}
+</style>
