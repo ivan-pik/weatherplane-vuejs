0f7862d04e537520919bd5ca21fa15435d48ce52
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2018-03-10 20:54:34 +0000

    New uiLabel component

---
+++ src/uiComponents/label.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="uiLabel">
+		<slot />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'uiLabel',
+		data () {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.uiLabel {
+		font-size: 0.7rem;
+		display: block;
+	}
+</style>
