1f94bba2005c2f5dc8c4e7f5408a9c51cc86a1f2
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2018-03-10 20:55:47 +0000

    new uiButtonGroup component

---
+++ src/uiComponents/layout/buttonGroup.vue
@@ -0,0 +1,36 @@
+<template>
+	<div
+		class="uiButtonGroup"
+	>
+		<slot />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'uiGroup',
+		props: {
+
+		},
+		data () {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import '~globalVars';
+	.uiButtonGroup {
+		display: flex;
+		min-height: 40px;
+		.uiButton:not(:last-child) {
+			margin-right: 1px;
+		}
+	}
+
+	.uiButtonGroup--gutter {
+		padding: $widthGridLargerSpace;
+	}
+</style>
