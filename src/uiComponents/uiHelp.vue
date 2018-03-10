6c52f0fec4a8771b22b9483ce132f3d21f870d99
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2018-03-10 20:56:14 +0000

    New uiHelp component

---
+++ src/uiComponents/uiHelp.vue
@@ -0,0 +1,32 @@
+<template>
+	<div
+		class="uiHelp"
+	>
+		{{text}}
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'uiHelp',
+		props: {
+			text: String
+		},
+		data () {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import '~globalVars';
+	.uiHelp {
+		display: block;
+		font-size: 0.7rem;
+		padding: 1em 0 0 0;
+		color: #c70000;
+	}
+
+</style>
