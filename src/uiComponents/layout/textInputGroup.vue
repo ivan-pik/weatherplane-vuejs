b02e683f674b4eb0c04769bcbbb34371c6afbcda
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2018-03-10 20:55:38 +0000

    New uiTextInputGroup component

---
+++ src/uiComponents/layout/textInputGroup.vue
@@ -0,0 +1,28 @@
+<template>
+	<div
+		class="uiTextInputGroup"
+	>
+		<slot />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'uiTextInputGroup',
+		data () {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import '~globalVars';
+
+	.uiTextInputGroup {
+		display: block;
+		margin: 0 0 1em 0;
+		position: relative;
+	}
+</style>
