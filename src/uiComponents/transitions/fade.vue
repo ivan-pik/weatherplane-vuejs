a4ac1eaa227b0289d1005f7573b3d42c3c2b0388
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2018-03-10 20:56:29 +0000

    New uiTransitionFade component

---
+++ src/uiComponents/transitions/fade.vue
@@ -0,0 +1,25 @@
+<template>
+	<transition name="fade">
+		<slot/>
+	</transition>
+</template>
+
+<script>
+	export default {
+		name: 'ui-transition-fade',
+		data () {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.fade-enter-active, .fade-leave-active {
+		transition: opacity 1s
+	}
+	.fade-enter, .fade-leave-to {
+		opacity: 0;
+	}
+</style>
